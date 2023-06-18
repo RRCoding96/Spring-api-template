dd0b132c718d3f5dcd3bbc93aac29cdc295459c9
김윤욱 <reznov03048@gmail.com>
2023-06-18 17:17:19 +0900

    feat: 인증 인터셉터 & annotation으로 인증

--- src/main/java/com/app/global/config/web/WebConfig.java
+++ src/main/java/com/app/global/config/web/WebConfig.java
@@ -1,12 +1,24 @@
 package com.app.global.config.web;
 
+import com.app.global.interceptor.AuthenticationInterceptor;
+import com.app.global.resolver.memberinfo.MemberInfoArgumentResolver;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
+    private final MemberInfoArgumentResolver memberInfoArgumentResolver;
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
@@ -22,5 +34,21 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.OPTIONS.name()
                 )
                 .maxAge(3600); // preflight 시간 설정
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor)
+                .order(1) // 가장 먼저 인증 인터셉터가 실행
+                .addPathPatterns("/api/**")
+                .excludePathPatterns("/api/oauth/login",
+                        "/api/access-token/issue",
+                        "/api/logout",
+                        "/api/health");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(memberInfoArgumentResolver);
     }
 }
